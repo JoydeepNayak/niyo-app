d91761c870711f25390d433aff7e7639aab04146
Joydeep.N <joydeepnayak123@gmail.com>
2020-09-20 22:25:48 +0530

    Updates

---
+++ precache-manifest.865b8ec04e4f5a13cae50a833d914fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87ac0fc2f2eb4560bb7b080e71a2c95",
+    "url": "/movie-info/index.html"
+  },
+  {
+    "revision": "aea0ef025cfa33755be4",
+    "url": "/movie-info/static/css/main.6dfcd059.chunk.css"
+  },
+  {
+    "revision": "e2043f0d730c49126bd0",
+    "url": "/movie-info/static/js/2.f2eb5761.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/movie-info/static/js/2.f2eb5761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea0ef025cfa33755be4",
+    "url": "/movie-info/static/js/main.379e06f3.chunk.js"
+  },
+  {
+    "revision": "9e6ec6c5c4ae6bd3e3e7",
+    "url": "/movie-info/static/js/runtime-main.9e12b8d8.js"
+  }
+]);
